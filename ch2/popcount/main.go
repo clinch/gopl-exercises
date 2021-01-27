2299e230b62f45a1215fc15d245d520cdfd88731
Chris Inch <chris.inch@shopify.com>
2021-01-27 15:12:57 -0500

    Reorged

--- ch2/popcount/main.go
+++ ch2/popcount/main.go
@@ -1,13 +1,11 @@
 // Package popcount will calculate the popcount of integers
-package main
+package popcount
 
 import (
 	"fmt"
-
-	"popcount"
 )
 
 func main() {
-	fmt.Println(popcount.PopCount(45))
-	fmt.Println(popcount.PopCountLoop(45))
+	fmt.Println(PopCount(45))
+	fmt.Println(PopCountLoop(45))
 }
